d4655968236e86e42ceaa29233e455f0e4cc17e3
Gabriele Del Monte <gabriele.delmonte90@gmail.com>
2018-09-14 17:55:25 +0200

    First basic commit

---
+++ src/main/java/io.openshift.booster/Booster.java
@@ -0,0 +1,13 @@
+package io.openshift.booster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Booster {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Booster.class, args);
+    }
+
+}
---
+++ src/main/java/io.openshift.booster/controller/MainRestController.java
@@ -0,0 +1,13 @@
+package io.openshift.booster.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainRestController {
+
+    @GetMapping
+    public String test() {
+        return "Test";
+    }
+}
